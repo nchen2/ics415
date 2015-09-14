bf53a8aadc493efb8663f2f674633b0ef8051d48
nchen2 <noelle_123@dhcp-168-105-85-198.wireless.manoa.hawaii.edu>
2015-09-14 09:55:52 -1000

    09/14

---
+++ IC/w4c1/change.js
@@ -0,0 +1,45 @@
+function changecolor() {
+  document.getElementById("headerbar").style.color="white";
+  document.getElementById("headerbar").style.backgroundColor="lightskyblue";
+}
+
+function changeback() {
+  document.getElementById("headerbar").style.color="black";
+  document.getElementById("headerbar").style.backgroundColor="white";
+}
+
+function white() {
+  document.getElementById("main").style.backgroundColor="white";
+}
+
+function yellow() {
+  document.getElementById("main").style.backgroundColor="yellow";
+}
+
+function green() {
+  document.getElementById("main").style.backgroundColor="green";
+}
+
+function sidewhite() {
+  document.getElementById("sidebar").style.backgroundColor="white";
+  document.getElementById("body").style.backgroundColor="white";
+}
+
+function sideyellow() {
+  document.getElementById("sidebar").style.backgroundColor="yellow";
+  document.getElementById("body").style.backgroundColor="yellow";
+}
+
+function sidegreen() {
+  document.getElementById("sidebar").style.backgroundColor="green";
+  document.getElementById("body").style.backgroundColor="green";
+}
+
+function revertmain() {
+  document.getElementById("main").style.backgroundColor="aliceblue";
+}
+
+function revertsidebar() {
+  document.getElementById("sidebar").style.backgroundColor="lightgray";
+  document.getElementById("body").style.backgroundColor="lightgray";
+}
